ff02c4467b96dff8688b7bc079036a4d807d4e01
ayrbox <srdx7@hotmail.com>
2020-05-09 23:33:35 +0100

    Timeline section.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import Layout from "../components/Layout";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import NewsSection from "../components/NewsSections";
+import TimeLine from "../components/TimeLine";
 
 const IndexPage = () => {
   return (
     <Layout>
       <Hero />
       <NewsSection />
+      <TimeLine />
       <ContactSection />
       <Footer />
     </Layout>
